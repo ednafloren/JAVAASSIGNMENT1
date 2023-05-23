64861bad4e6b842681d3f6c10b9ee317d5a87c4e
ednafloren <107610678+ednafloren@users.noreply.github.com>
2023-05-23 12:17:51 +0300

    Update and rename arraysum.java to ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,7 +1,7 @@
-public class arraysum {  
+public class ArraySum {  
     public static void main(String[] args) {  
         //Initialize array  
-        int [] arr = new int [] {4,2,4,3};  
+        int [] arr = {4,2,4,3};  
         int sum = 0;  
         //Loop through the array to calculate sum of elements  
         for (int i = 0; i < arr.length; i++) {  
